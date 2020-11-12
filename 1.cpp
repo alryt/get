a0059cfb2802be40134bfb987e8920296373d6f6
alryt <rytova.ae@phystech.edu>
2020-11-12 10:44:49 +0300

    ргргр

---
+++ 1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+	int n, i, sum=0;
+    float sa = 0;
+    cin >> n;
+    int a[1000];
+    for( i = 0; i < n; i++){
+        cin >> a[i];
+    }
+    for( i = 0; i < n; i++){
+        sum+=a[i];
+    }
+    sa=float(sum) / float(n);
+    for( i = 0; i < n; i++){
+        if (a[i] > sa){
+            cout << a[i] << " ";
+        }
+    }
+    return 0;
+}
